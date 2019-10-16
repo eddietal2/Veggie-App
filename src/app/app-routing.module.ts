bf148de90fcf4235a7cf6440ecd495fab46d6d87
Eddie Taliaferro <eddielacrosse2@gmail.com>
2019-10-16 02:34:30 -0400

    adding signup verification and thank you pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,9 @@ const routes: Routes = [
   { path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' },
   { path: 'home',
   loadChildren: () =>
-    import('./pages/home/home.module').then(m => m.HomePageModule) }
+    import('./pages/home/home.module').then(m => m.HomePageModule) },
+  { path: 'sign-up-verification', loadChildren: './pages/sign-up-verification/sign-up-verification.module#SignUpVerificationPageModule' },
+  { path: 'sign-up-thank-you', loadChildren: './pages/sign-up-thank-you/sign-up-thank-you.module#SignUpThankYouPageModule' }
 ];
 @NgModule({
   imports: [
