afa489b70c371d50b15bbc01fed7ec5ab801f681
Eddie Taliaferro <eddielacrosse2@gmail.com>
2019-10-16 14:30:42 -0400

    fixed issue with tabs not showing up / import TabsPageModule to app.routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: './pages/landing-page/landing-page.module#LandingPagePageModule'
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
+  { path: 'landing-page', loadChildren: './landing-page/landing-page.module#LandingPagePageModule' },
   { path: 'landing-page', loadChildren: './pages/landing-page/landing-page.module#LandingPagePageModule' },
   { path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' },
-  { path: 'home',
-  loadChildren: () =>
-    import('./pages/home/home.module').then(m => m.HomePageModule) },
   { path: 'sign-up-verification', loadChildren: './pages/sign-up-verification/sign-up-verification.module#SignUpVerificationPageModule' },
-  { path: 'sign-up-thank-you', loadChildren: './pages/sign-up-thank-you/sign-up-thank-you.module#SignUpThankYouPageModule' }
+  { path: 'sign-up-thank-you', loadChildren: './pages/sign-up-thank-you/sign-up-thank-you.module#SignUpThankYouPageModule' },
+  { path: 'home',
+   loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule) }
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
